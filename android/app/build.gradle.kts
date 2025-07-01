073ab3d56c2c4aac94a2cb0e3a142c5f797ec502
maxime louvel <maxime.louvel@bag-era.fr>
2025-07-01 16:00:37 +0200

    android renaming

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "app.flowit.flowit_app"
+    namespace = "app.towdow.towdow_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.flowit.flowit_app"
+        applicationId = "app.towdow.towdow_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
