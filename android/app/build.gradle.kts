5eea18e8904f9f893e8413a5764b2a4676c17065
maxime louvel <maxime.louvel@bag-era.fr>
2025-07-02 08:18:30 +0200

    fix android build by setting obsolete JavaVersion.VERSION_1_8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,18 @@ plugins {
 android {
     namespace = "app.towdow.towdow_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // For AGP 4.1+
+        isCoreLibraryDesugaringEnabled = true
+        // Sets Java compatibility to Java 8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
@@ -41,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
