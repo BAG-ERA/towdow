d540d132839e74cd0a39a7d5976fd73b9fca88b0
maxime louvel <maxime.louvel@bag-era.fr>
2025-07-03 09:09:35 +0200

    CI: fix linux build

--- CI_scripts/linux/build_app_image.sh
+++ CI_scripts/linux/build_app_image.sh
@@ -5,18 +5,17 @@ ARCH=x86_64
 APP_IMAGE_TOOL=appimagetool-${ARCH}.AppImage
 APPIMAGE_OUTPUT=${3-build/linux/x64/release/${APP_NAME}.AppImage}
 
-
 rm -rf ${APP_DIR}
 mkdir -p ${APP_DIR}
 
 echo "getting appimagetool"
 
-curl -L -o ${APP_IMAGE_TOOL} "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-${ARCH}.AppImage"
-chmod +x ${APP_IMAGE_TOOL}
+#curl -L -o ${APP_IMAGE_TOOL} "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-${ARCH}.AppImage"
+#chmod +x ${APP_IMAGE_TOOL}
 
 echo "building app"
 flutter pub get
-flutter build linux --release --build-name=$1 --build-number=$2
+flutter build linux --release --build-name="$1" --build-number="$2"
 
 echo "preparing AppDir ${APP_DIR}"
 cp -r build/linux/x64/release/bundle/* ${APP_DIR}/
@@ -33,6 +32,8 @@ Categories=Utility
 cp assets/icons/logo/Flow-it_Default.png ${APP_DIR}/icon.png
 
 echo "create app image"
-./${APP_IMAGE_TOOL} ${APP_DIR} ${APPIMAGE_OUTPUT}
+./"$(dirname "$0")"/"${APP_IMAGE_TOOL}" "${APP_DIR}" "${APPIMAGE_OUTPUT}"
+
+ls -l "${APPIMAGE_OUTPUT}"
 
 echo "done"
