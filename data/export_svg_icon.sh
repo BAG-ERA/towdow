781e83f79437270d5708c149fc817712f2307dea
maxime louvel <maxime.louvel@bag-era.fr>
2025-08-26 14:08:31 -0400

    WIP: working flatpack for app.towdow.app

---
+++ data/export_svg_icon.sh
@@ -0,0 +1,10 @@
+
+
+# transparent PNGs, square; adjust the source path to your SVG
+for s in 64 128 256; do
+  mkdir -p "data/icons/hicolor/${s}x${s}/apps/"
+  inkscape assets/icons/logo/Flow-it_icon-Default.svg \
+    --export-type=png \
+    --export-filename="data/icons/hicolor/${s}x${s}/apps/app.towdow.app.png" \
+    -w $s -h $s
+done
