3f7e5c51689302ab57d9ee5e00af5e003310caf4
maxime louvel <maxime.louvel@bag-era.fr>
2025-08-26 10:35:35 -0400

    clean CI job, add debug in CI script

--- CI_scripts/linux/build_snapcraft_from_appimage.sh
+++ CI_scripts/linux/build_snapcraft_from_appimage.sh
@@ -1,4 +1,7 @@
 echo "building $CI_COMMIT_TAG version for snapcraft"
 sed -i "s/0\.0\.0/${CI_COMMIT_TAG}/g" snapcraft.yaml
+ls
 cp build/linux/x64/release/towdow_app-$CI_COMMIT_TAG.AppImage towdow_app-$CI_COMMIT_TAG.AppImage
+echo "copied AppImage at root level:"
+ls
 snapcraft pack
