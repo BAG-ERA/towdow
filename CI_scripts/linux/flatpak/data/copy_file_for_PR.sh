02e5a9c5ad4f0f417fbc6a1da581932f454ede1b
maxime louvel <maxime.louvel@bag-era.fr>
2025-08-27 17:48:30 -0400

    WIP: script to create PR

---
+++ CI_scripts/linux/flatpak/data/copy_file_for_PR.sh
@@ -0,0 +1,13 @@
+flathub_repo_dir=${1-/home/maxime/work/external_repos/flathub}
+VERSION=${2-"1.0.0"}
+cp app.towdow.TowDow.yaml "${flathub_repo_dir}/"
+cp app.towdow.TowDow.yaml "${flathub_repo_dir}/"
+mkdir -p "${flathub_repo_dir/build/linux/x64/release/}"
+cp "build/linux/x64/release/towdow_app-$VERSION.AppImage" "${flathub_repo_dir}/build/linux/x64/release/"
+mkdir -p "${flathub_repo_dir}/CI_scripts/linux/flatpak/data/"
+cp -r CI_scripts/linux/flatpak/data/icons "${flathub_repo_dir}/CI_scripts/linux/flatpak/data/"
+cp -r CI_scripts/linux/flatpak/data/app.towdow.TowDow.desktop "${flathub_repo_dir}/CI_scripts/linux/flatpak/data/"
+cp -r CI_scripts/linux/flatpak/data/app.towdow.TowDow.metainfo.xml "${flathub_repo_dir}/CI_scripts/linux/flatpak/data/"
+# update version in files
+sed -i "s/0\.0\.0/${VERSION}/g" "${flathub_repo_dir}/app.towdow.TowDow.yaml"
+sed -i "s/0\.0\.0/${VERSION}/g" "${flathub_repo_dir}/CI_scripts/linux/flatpak/data/app.towdow.TowDow.metainfo.xml"
