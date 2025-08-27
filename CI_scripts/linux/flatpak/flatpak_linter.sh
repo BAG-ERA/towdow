e4208f423ff988b11dd3919a340e21e1856a4f0b
maxime louvel <maxime.louvel@bag-era.fr>
2025-08-27 14:51:56 -0400

    WIP: flatpak linter

--- CI_scripts/linux/flatpak/flatpak_linter.sh
+++ CI_scripts/linux/flatpak/flatpak_linter.sh
@@ -1,7 +1,11 @@
-flatpak-builder --force-clean build-dir app.towdow.TowDow.yaml \
---default-branch=stable \
---mirror-screenshots-url=https://dl.flathub.org/media/ || exit $?
+rm -rf repo build-dir .flatpak-builder
+flatpak-builder \
+  --force-clean \
+  --repo=repo \
+  --default-branch=stable \
+  --mirror-screenshots-url=https://dl.flathub.org/media/ \
+  build-dir \
+  app.towdow.TowDow.yaml || exit $?
 flatpak build build-dir appstreamcli validate /app/share/metainfo/app.towdow.TowDow.metainfo.xml  || exit $?
-flatpak build-export --update-appstream repo build-dir || exit $?
 flatpak build-update-repo --generate-static-deltas repo || exit $?
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo repo || exit $?
