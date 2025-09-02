65c376293bbf9055e6ef4f453e7f1db152c5ec21
maxime louvel <maxime.louvel@bag-era.fr>
2025-09-02 14:16:27 -0400

    CI: increase  test timeout

--- test/services/sync_queue_processing_test.dart
+++ test/services/sync_queue_processing_test.dart
@@ -1,4 +1,6 @@
 // Tests for SyncService queue processing (create/update/delete)
+@Timeout(Duration(seconds: 60))
+library;
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
