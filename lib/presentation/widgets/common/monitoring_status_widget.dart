a07f89084c8f99ae9e6ebc0b8c29d3cde595134c
maxime louvel <maxime.louvel@bag-era.fr>
2025-09-15 14:42:28 -0400

    replace static icon with animation when performing monitoring

--- lib/presentation/widgets/common/monitoring_status_widget.dart
+++ lib/presentation/widgets/common/monitoring_status_widget.dart
@@ -3,17 +3,47 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../data/providers/providers_viewmodels.dart';
 import '../../viewmodels/monitoring_status_viewmodel.dart';
 
-class MonitoringStatusWidget extends ConsumerWidget {
+class MonitoringStatusWidget extends ConsumerStatefulWidget {
   final bool compact;
   const MonitoringStatusWidget({super.key, this.compact = false});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MonitoringStatusWidget> createState() => _MonitoringStatusWidgetState();
+}
+
+class _MonitoringStatusWidgetState extends ConsumerState<MonitoringStatusWidget> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final state = ref.watch(monitoringStatusViewModelProvider);
 
     IconData icon;
     Color color;
     String label;
+
+    // Control animation according to state
+    if (state == MonitoringUiState.inProgress) {
+      if (!_controller.isAnimating) {
+        _controller.repeat();
+      }
+    } else {
+      if (_controller.isAnimating) {
+        _controller.stop();
+      }
+    }
 
     switch (state) {
       case MonitoringUiState.inProgress:
@@ -33,12 +63,18 @@ class MonitoringStatusWidget extends ConsumerWidget {
         break;
     }
 
-    if (compact) {
+    Widget iconWidget = Icon(icon, color: color, size: widget.compact ? 20 : 18);
+
+    if (state == MonitoringUiState.inProgress) {
+      iconWidget = RotationTransition(turns: _controller, child: iconWidget);
+    }
+
+    if (widget.compact) {
       return Tooltip(
         message: label,
         child: Semantics(
           label: label,
-          child: Icon(icon, color: color, size: 20),
+          child: iconWidget,
         ),
       );
     }
@@ -53,7 +89,7 @@ class MonitoringStatusWidget extends ConsumerWidget {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(icon, color: color, size: 18),
+          iconWidget,
           const SizedBox(width: 8),
           Text(
             label,
